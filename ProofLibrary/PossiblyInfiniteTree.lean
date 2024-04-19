b356ac2872246f2bc4e082bf58189cf089e7491c
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-04-19 16:57:02 +0200

    Work on def children in possibly infinite tree

--- ProofLibrary/PossiblyInfiniteTree.lean
+++ ProofLibrary/PossiblyInfiniteTree.lean
@@ -12,7 +12,7 @@ structure PossiblyInfiniteTree (α : Type u) where
     | none => True
     | some list => match (list.map (fun ni => ni.number_of_children)).sum with
       | Nat.zero => (data.infinite_list (depth + 1)) = none
-      | Nat.succ n => (data.infinite_list (depth + 1)) = some next_layer ∧ next_layer.length = Nat.succ n
+      | Nat.succ n => ∃ next_layer, (data.infinite_list (depth + 1)) = some next_layer ∧ next_layer.length = Nat.succ n
 
 structure NodeInPossiblyInfiniteTree (α) where
   tree : PossiblyInfiniteTree α
@@ -47,7 +47,6 @@ namespace NodeInPossiblyInfiniteTree
     simp [node_info]
     apply List.listGetInToSet
 
-  /-
   def children (node : NodeInPossiblyInfiniteTree α) : List (NodeInPossiblyInfiniteTree α) :=
     let current_layer := node.layer
     let next_layer_opt := node.tree.data.infinite_list (node.depth + 1)
@@ -55,6 +54,7 @@ namespace NodeInPossiblyInfiniteTree
     let node_info := node.node_info
     let number_of_children := node_info.number_of_children
     let layer_mapped := current_layer.map (fun ni => ni.number_of_children)
+    let number_of_children_before := (current_layer_before_this.map (fun ni => ni.number_of_children)).sum
 
     have no_children_in_mapped_layer : number_of_children ∈ layer_mapped.toSet := by
       apply List.mappedElemInMappedList
@@ -64,16 +64,44 @@ namespace NodeInPossiblyInfiniteTree
       apply List.everyElementLeSum
       exact no_children_in_mapped_layer
 
-    match equality : number_of_children with
+    match equality_noc : node_info.number_of_children with
       | Nat.zero => List.nil
       | Nat.succ n =>
-        have consistency := node.tree.consistency node.depth
-        -- TODO: continue here
-        have something := by
-          simp [nodeLayerIsLayerAtDepth] at consistency
+        let sumNeZero : layer_mapped.sum ≠ 0 := by
+          apply Nat.not_eq_zero_of_lt
+          apply Nat.lt_of_lt_of_le
+          apply Nat.zero_lt_of_ne_zero
+          exact Nat.succ_ne_zero n
+          rw [←equality_noc]
+          exact no_children_le_sum_layer_mapped
 
-        sorry
-  -/
+        match equality_sum : layer_mapped.sum with
+          | Nat.zero => absurd equality_sum sumNeZero
+          | Nat.succ m =>
+            let consistency := node.tree.consistency node.depth
+            have nextLayerExists : next_layer_opt ≠ none := by
+              simp [nodeLayerIsLayerAtDepth] at consistency
+              simp [equality_sum] at consistency
+              cases consistency with | intro _ h =>
+                have layer_exists := h.left
+                exact Option.someNotNone layer_exists
+
+            let next_layer := next_layer_opt.unwrap nextLayerExists
+            let child_information := (next_layer.drop number_of_children_before).take number_of_children
+
+            child_information.enum.map (fun (i, info) => {
+              tree := node.tree,
+              depth := node.depth + 1,
+              position_in_layer := number_of_children_before + i,
+              layer_exists := by
+                exists next_layer
+                simp
+                rw [Option.someRevertsUnwrap]
+              layer_large_enough := by
+                intro layer h
+                -- TODO: continue here
+                sorry
+            })
 
   /- TODO: maybe also define siblings similarly, i.e. as node.layer but with NodeInPossiblyInfiniteTree instead of just NodeInformation -/
 end NodeInPossiblyInfiniteTree
