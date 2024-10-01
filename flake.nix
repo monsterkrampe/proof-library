57b5dda5aa0263af4ae290a5a2709e39ac37a0d6
monsterkrampe <monsterkrampe@users.noreply.github.com>
2024-10-01 08:45:13 +0200

    Add flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Proof Library";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.elan
+            ];
+          };
+        }
+      );
+}
