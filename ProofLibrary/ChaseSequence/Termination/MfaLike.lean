4bc743fe2171d0254b5a691e20549bbcd366f760
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-01-23 14:12:01 +0100

    Show: all skolem chase branches yield same result

--- ProofLibrary/ChaseSequence/Termination/MfaLike.lean
+++ ProofLibrary/ChaseSequence/Termination/MfaLike.lean
@@ -149,8 +149,6 @@ namespace KnowledgeBase
                       rw [List.getElem_map, ← List.inIffInToSet] at h
                       have : i.val = 0 := by
                         have isLt := i.isLt
-                        /- unfold PreTrigger.result at isLt -/
-                        /- simp only [List.length_map] at isLt -/
                         simp only [← PreTrigger.head_length_eq_mapped_head_length] at isLt
                         rw [det _ trg.property, Nat.lt_one_iff] at isLt
                         exact isLt
@@ -161,14 +159,151 @@ namespace KnowledgeBase
                     exact h
     . intro h
       rcases h with ⟨n, h⟩
-      induction n with
+      induction n generalizing f with
       | zero =>
         exists 0
         rw [cb.database_first, Option.is_some_and]
         exact h
       | succ n ih =>
-        -- not as "easy" as the other direcition, we need to invoke fairness somehow and likely need to show that only finitely many triggers are active at the same time...
-        sorry
+        -- we need to invoke fairness somehow
+        unfold parallelSkolemChase at h
+        cases h with
+        | inl h => exact ih _ h
+        | inr h =>
+          rcases h with ⟨trg, trg_active, f_mem⟩
+
+          have trg_loaded_somewhere : ∃ n, (cb.branch.infinite_list n).is_some_and (fun node => trg.val.loaded node.fact.val) := by
+            have : ∀ (l : List (Fact sig)), (∀ e, e ∈ l -> e ∈ trg.val.mapped_body) -> ∃ n, (cb.branch.infinite_list n).is_some_and (fun node => (∀ e, e ∈ l -> e ∈ node.fact.val)) := by
+              intro l l_sub
+              induction l with
+              | nil => exists 0; rw [cb.database_first, Option.is_some_and]; simp
+              | cons hd tl ih_inner =>
+                have from_ih := ih_inner (by intro f f_mem; apply l_sub; simp [f_mem])
+                rcases from_ih with ⟨n_from_ih, from_ih⟩
+
+                cases eq_from_ih : cb.branch.infinite_list n_from_ih with
+                | none => rw [eq_from_ih, Option.is_some_and] at from_ih; simp at from_ih
+                | some node_from_ih =>
+                rw [eq_from_ih, Option.is_some_and] at from_ih
+
+                have from_hd := ih hd (by apply trg_active.left; rw [← List.inIffInToSet]; apply l_sub; simp)
+                rcases from_hd with ⟨n_from_hd, from_hd⟩
+
+                cases eq_from_hd : cb.branch.infinite_list n_from_hd with
+                | none => rw [eq_from_hd, Option.is_some_and] at from_hd; simp at from_hd
+                | some node_from_hd =>
+                rw [eq_from_hd, Option.is_some_and] at from_hd
+
+                cases Decidable.em (n_from_ih ≤ n_from_hd) with
+                | inl le =>
+                  exists n_from_hd
+                  rw [eq_from_hd, Option.is_some_and]
+                  intro f f_mem
+                  simp at f_mem
+                  cases f_mem with
+                  | inl f_mem => rw [f_mem]; exact from_hd
+                  | inr f_mem =>
+                    rcases Nat.exists_eq_add_of_le le with ⟨diff, le⟩
+                    have subsetAllFollowing := chaseBranchSetIsSubsetOfAllFollowing cb n_from_ih diff
+                    simp only [eq_from_ih] at subsetAllFollowing
+                    rw [← le, eq_from_hd, Option.is_none_or] at subsetAllFollowing
+                    apply subsetAllFollowing
+                    apply from_ih; exact f_mem
+                | inr lt =>
+                  simp at lt
+                  have le := Nat.le_of_lt lt
+                  exists n_from_ih
+                  rw [eq_from_ih, Option.is_some_and]
+                  intro f f_mem
+                  simp at f_mem
+                  cases f_mem with
+                  | inr f_mem => apply from_ih; exact f_mem
+                  | inl f_mem =>
+                    rcases Nat.exists_eq_add_of_le le with ⟨diff, le⟩
+                    have subsetAllFollowing := chaseBranchSetIsSubsetOfAllFollowing cb n_from_hd diff
+                    simp only [eq_from_hd] at subsetAllFollowing
+                    rw [← le, eq_from_ih, Option.is_none_or] at subsetAllFollowing
+                    apply subsetAllFollowing
+                    rw [f_mem]; exact from_hd
+
+            specialize this trg.val.mapped_body (by simp)
+            rcases this with ⟨n, this⟩
+            exists n
+            cases eq : cb.branch.infinite_list n with
+            | none => rw [eq, Option.is_some_and] at this; simp at this
+            | some node =>
+              rw [Option.is_some_and]
+              rw [eq, Option.is_some_and] at this
+              intro f
+              rw [← List.inIffInToSet]
+              apply this
+          rcases trg_loaded_somewhere with ⟨loaded_n, trg_loaded_somewhere⟩
+          cases eq_node_loaded : cb.branch.infinite_list loaded_n with
+          | none => rw [eq_node_loaded, Option.is_some_and] at trg_loaded_somewhere; simp at trg_loaded_somewhere
+          | some node_loaded =>
+          rw [eq_node_loaded, Option.is_some_and] at trg_loaded_somewhere
+
+          have fair := cb.fairness trg
+          rcases fair with ⟨fairness_n, fair⟩
+          cases eq_node_fairness : cb.branch.infinite_list fairness_n with
+          | none => rw [eq_node_fairness, Option.is_some_and] at fair; simp at fair
+          | some node_fairness =>
+          rw [eq_node_fairness, Option.is_some_and] at fair
+
+          cases Decidable.em (loaded_n ≤ fairness_n) with
+          | inl le =>
+            exists fairness_n
+            rw [eq_node_fairness, Option.is_some_and]
+            have trg_not_active := fair.left
+            unfold Trigger.active at trg_not_active
+            simp only [not_and, Classical.not_not] at trg_not_active
+
+            have trg_loaded : trg.val.loaded node_fairness.fact.val := by
+              intro f f_mem
+              rcases Nat.exists_eq_add_of_le le with ⟨diff, le⟩
+              have subsetAllFollowing := chaseBranchSetIsSubsetOfAllFollowing cb loaded_n diff
+              simp only [eq_node_loaded] at subsetAllFollowing
+              rw [← le, eq_node_fairness, Option.is_none_or] at subsetAllFollowing
+              apply subsetAllFollowing
+              apply trg_loaded_somewhere
+              exact f_mem
+
+            specialize trg_not_active trg_loaded
+            rcases trg_not_active with ⟨disj_index, trg_not_active⟩
+            apply trg_not_active
+            have disj_index_zero : disj_index.val = 0 := by
+              have isLt := disj_index.isLt
+              simp only [← PreTrigger.head_length_eq_mapped_head_length] at isLt
+              rw [det _ trg.property, Nat.lt_one_iff] at isLt
+              exact isLt
+            unfold PreTrigger.result at f_mem
+            rw [List.getElem_map, ← List.inIffInToSet] at f_mem
+            rw [← List.inIffInToSet, List.get_eq_getElem]
+            simp only [disj_index_zero]
+            exact f_mem
+          | inr lt =>
+            simp at lt
+
+            exists loaded_n
+            rw [eq_node_loaded, Option.is_some_and]
+            have trg_not_active := fair.right loaded_n lt
+            rw [eq_node_loaded, Option.is_none_or] at trg_not_active
+            unfold Trigger.active at trg_not_active
+            simp only [not_and, Classical.not_not] at trg_not_active
+
+            specialize trg_not_active trg_loaded_somewhere
+            rcases trg_not_active with ⟨disj_index, trg_not_active⟩
+            apply trg_not_active
+            have disj_index_zero : disj_index.val = 0 := by
+              have isLt := disj_index.isLt
+              simp only [← PreTrigger.head_length_eq_mapped_head_length] at isLt
+              rw [det _ trg.property, Nat.lt_one_iff] at isLt
+              exact isLt
+            unfold PreTrigger.result at f_mem
+            rw [List.getElem_map, ← List.inIffInToSet] at f_mem
+            rw [← List.inIffInToSet, List.get_eq_getElem]
+            simp only [disj_index_zero]
+            exact f_mem
 
 end KnowledgeBase
 
