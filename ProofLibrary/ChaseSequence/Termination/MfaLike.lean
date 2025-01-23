9734183f02cd317acefc7d980adf1e994f452bc5
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-01-23 08:52:20 +0100

    Finish Critical Instance definition

--- ProofLibrary/ChaseSequence/Termination/MfaLike.lean
+++ ProofLibrary/ChaseSequence/Termination/MfaLike.lean
@@ -6,25 +6,34 @@ def criticalInstance (rs : RuleSet sig) (finite : rs.rules.finite) (special_cons
   ⟨fun f => f.predicate ∈ rs.predicates ∧ ∀ t, t ∈ f.terms -> t = special_const, by
     have preds_finite := rs.predicates_finite_of_finite finite
     rcases preds_finite with ⟨l, nodup, eq⟩
-    exists l.map (fun p => {
+    exists (l.map (fun p => {
       predicate := p
       terms := List.repeat special_const (sig.arity p)
       arity_ok := by rw [List.length_repeat]
-    })
+    })).eraseDupsKeepRight
     constructor
-    . sorry
+    . apply List.nodup_eraseDupsKeepRight
     . intro f
-      simp
+      rw [List.mem_eraseDupsKeepRight_iff]
+      simp [Set.element]
       constructor
       . intro h
         rcases h with ⟨p, p_mem, f_eq⟩
         rw [← f_eq]
-        simp [Set.element]
         rw [eq] at p_mem
         constructor
         . exact p_mem
         . intro t
-          sorry
-      sorry
+          apply List.mem_repeat
+      . intro h
+        exists f.predicate
+        constructor
+        . rw [eq]; exact h.left
+        . rw [FunctionFreeFact.ext_iff]
+          simp
+          rw [List.repeat_eq_iff_all_val]
+          constructor
+          . exact f.arity_ok
+          . exact h.right
   ⟩
 
--- ProofLibrary/List.lean
+++ ProofLibrary/List.lean
@@ -384,6 +384,38 @@ theorem List.length_repeat (val : α) : ∀ n, (List.repeat val n).length = n :=
   | zero => simp [List.repeat]
   | succ n ih => simp [List.repeat]; exact ih
 
+theorem List.mem_repeat (val : α) (n : Nat) : ∀ val', val' ∈ (List.repeat val n) -> val' = val := by
+  induction n with
+  | zero => simp [List.repeat]
+  | succ n ih => simp [List.repeat]; exact ih
+
+theorem List.repeat_eq_iff_all_val (val : α) (n : Nat) (l : List α) : (List.repeat val n) = l ↔ (l.length = n ∧ ∀ e, e ∈ l -> e = val) := by
+  induction n generalizing l with
+  | zero => simp [List.repeat]; intro eq _; simp [eq]
+  | succ n ih =>
+    simp [List.repeat]
+    constructor
+    . intro h
+      rw [← h]
+      simp
+      apply (ih (List.repeat val n)).mp
+      rfl
+    . intro h
+      cases l with
+      | nil => simp at h
+      | cons hd tl =>
+        rw [List.cons_eq_cons]
+        constructor
+        . apply Eq.symm; apply h.right; simp
+        . apply (ih tl).mpr
+          constructor
+          . have := h.left
+            simp at this
+            exact this
+          . intro e e_mem
+            apply h.right
+            simp [e_mem]
+
 theorem List.all_eq_val_repeat [DecidableEq α] (val : α) : ∀ n, (List.repeat val n).all (fun e => e = val) = true := by
   intro n
   induction n with
