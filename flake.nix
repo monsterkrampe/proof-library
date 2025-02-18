206adbb678ae711bb901423c22195789de932d89
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-02-18 09:54:51 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Proof Library";
+  description = "Existential Rules in Lean";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
